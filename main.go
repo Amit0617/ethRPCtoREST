8542d99403c01a78cb1436e9dce61a9bbec37820
amit0617 <71893015+Amit0617@users.noreply.github.com>
2023-09-08 22:53:24 +0530

    wrapping eth_getTransactionByHash

--- main.go
+++ main.go
@@ -18,6 +18,17 @@ import (
 var RPC_URL string
 var client *ethclient.Client
 
+// A Block hash is 32 bytes long and hence 64 characters long plus 0x prefix
+var hashRegex = regexp.MustCompile(`^0x[0-9a-f]{64}$`)
+
+// A block number also allows default block identifiers such as "earliest", "latest" and "pending"
+// TODO: A block number can also be a decimal number without 0x prefix (my proposal)
+// A block number can also be a hex number with 0x prefix
+// A block number will always consist of a non-zero character after 0x, except for "0x0".
+
+// Regex to allow for default block identifiers
+var blockNumberRegex = regexp.MustCompile(`^0x([1-9a-f]+[0-9a-f]*|0)$|^earliest$|^latest$|^pending$|^safe$|^finalized$`)
+
 func main() {
 	app := fiber.New()
 	err := godotenv.Load(".env")
@@ -34,6 +45,7 @@ func main() {
 	}
 
 	app.Get("/eth/block/:identifier", getBlockByIdentifier)
+	app.Get("/eth/transaction/:hash", getTransactionByHash)
 
 	log.Fatal(app.Listen(":3000"))
 }
@@ -42,19 +54,7 @@ func main() {
 func getBlockByIdentifier(c *fiber.Ctx) error {
 	identifier := c.Params("identifier")
 
-	// Check if identifier is a block hash or block number
-	// A Block hash is 32 bytes long and hence 64 characters long without 0x prefix
-	// A block number will always consist of a non-zero character after 0x, except for "0x0".
-
-	// Use regex to check if identifier is a block hash or block number
-	blockHashRegex := regexp.MustCompile(`^0x[0-9a-f]{64}$`)
-
-	// A block number also allows default block identifiers such as "earliest", "latest" and "pending"
-	// TODO: A block number can also be a decimal number without 0x prefix (my proposal)
-	// A block number can also be a hex number with 0x prefix
-
-	// Regex to allow for default block identifiers
-	blockNumberRegex := regexp.MustCompile(`^0x([1-9a-f]+[0-9a-f]*|0)$|^earliest$|^latest$|^pending$|^safe$|^finalized$`)
+	blockHashRegex := hashRegex
 
 	// Check if identifier is a block hash or block number
 	if blockHashRegex.MatchString(identifier) {
@@ -78,7 +78,7 @@ func getBlockByHash(c *fiber.Ctx, hash string) error {
 	log.Println(blockHash)
 	block, err := client.HeaderByHash(context.Background(), blockHash)
 	if err != nil {
-		log.Fatal("Error fetching block info:", err)
+		log.Print("Error fetching block info:", err)
 	}
 	return c.JSON(block)
 }
@@ -99,7 +99,32 @@ func getBlockByNumber(c *fiber.Ctx, numberOrDefaultParameters string) error {
 	log.Println(blockNumber)
 	blockInfo, err := client.HeaderByNumber(context.Background(), blockNumber)
 	if err != nil {
-		log.Fatal("Error fetching block info:", err)
+		log.Print("Error fetching block info:", err)
 	}
 	return c.JSON(blockInfo)
+}
+
+func getTransactionByHash(c *fiber.Ctx) error {
+	hash := c.Params("hash")
+	// Check if hash is a valid transaction hash
+	transactionHashRegex := hashRegex
+	if transactionHashRegex.MatchString(hash) {
+		transactionHash := common.HexToHash(hash)
+		log.Println(transactionHash)
+		transaction, isPending, err := client.TransactionByHash(context.Background(), transactionHash)
+		if err != nil {
+			log.Print("Error fetching transaction info:", err)
+		}
+		if isPending {
+			// Return 202 Accepted status code
+			return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+				"info": "Transaction isn't mined yet",
+			})
+		}
+		return c.JSON(transaction)
+	} else {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid transaction hash",
+		})
+	}
 }
